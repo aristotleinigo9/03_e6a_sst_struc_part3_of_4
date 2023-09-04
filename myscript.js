3780ee655758084be45de9dca453eaed0309930d
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-04 13:06:57 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,619 @@
+var myQuestions = [
+  {
+    question: "1. A friend who just arrived from England gave me the pen __________ I am using.",
+    answers: {
+      a: 'whose',
+      b: 'whom',
+      c: 'which',
+      d: 'who'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "2. The basketball players were finally chosen. __________ is very short.",
+    answers: {
+      a: 'All of them',
+      b: 'Many of them',
+      c: 'One of them',
+      d: 'Two of them'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "3. Do you know __________?",
+    answers: {
+      a: 'whose bag is that',
+      b: 'whose bag that is',
+      c: 'who that bag is',
+      d: 'who is that bag'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "4. Her mother wonders if __________ pizza.",
+    answers: {
+      a: 'does Sarah want',
+      b: 'Sarah like',
+      c: 'Sarah wants',
+      d: 'Sarah want'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "5. The inspection crew can’t decide whether to __________ working or to leave the building.",
+    answers: {
+      a: 'continue',
+      b: 'continues',
+      c: 'continuing',
+      d: 'continued'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "6. __________ you tell those people will be useless now. They have made up their minds.",
+    answers: {
+      a: 'However',
+      b: 'Whichever',
+      c: 'Whatever',
+      d: 'Wherever'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "7. I’ll give you the __________ before I tell you the answer. ",
+    answers: {
+      a: 'guess',
+      b: "guess's",
+      c: "guess'",
+      d: 'guesses'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "8. The repairman fixed three different __________ this morning.",
+    answers: {
+      a: 'roof',
+      b: 'roofs',
+      c: 'rooves',
+      d: 'roofes'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "9. This bathroom is for ladies only.  It’s a __________room.",
+    answers: {
+      a: 'lady',
+      b: "lady's",
+      c: "ladies'",
+      d: 'ladies'
+
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "10. This dish has carrots in it.  It is a nice __________ dish.",
+    answers: {
+      a: 'carrot',
+      b: 'carrots',
+      c: "carrot's",
+      d: "carrots'"
+
+    },
+    correctAnswer: 'a'
+  },
+  
+  {
+    question: "11. People say that __________ is only skin deep.",
+    answers: {
+      a: 'beauty',
+      b: 'beauties',
+      c: "beauty's",
+      d: "beauties'"
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "12. Put a lot of __________ in the pan for frying chicken.",
+    answers: {
+      a: "oil",
+      b: 'oils',
+      c: "oil's",
+      d: "oils'"
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "13. We need __________ containers of juice for the children.",
+    answers: {
+      a: 'much',
+      b: 'a little',
+      c: 'several',
+      d: 'a bit of'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "14. __________ of the students in the class has a pen.",
+    answers: {
+      a: 'Some',
+      b: 'Many',
+      c: 'Several',
+      d: 'Each'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "15. Winning at sports __________ difficult for those who are not good athletes.",
+    answers: {
+      a: "are",
+      b: "is",
+      c: "be",
+      d: "have"
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "16. The number of toys in those two toy shops __________ 3,000.",
+    answers: {
+      a: 'is',
+      b: 'are',
+      c: 'be',
+      d: 'have'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "17. There __________ a desk, two blackboards, and several chairs in that room yesterday.",
+    answers: {
+      a: 'are',
+      b: 'was',
+      c: 'be',
+      d: 'were'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "18. Genetics __________ the study of how living things develop according to the effects of those substances passed on in the cells from the parents.",
+    answers: {
+      a: 'are',
+      b: 'be',
+      c: 'have',
+      d: 'is'
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "19. The professor brought __________ own book that she had written to class.",
+    answers: {
+      a: "his",
+      b: "our",
+      c: "her",
+      d: 'theirs'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "20. Anyone who doesn’t have __________ workbook will not be allowed to attend Mr. Smith’s class at the ITC.",
+    answers: {
+      a: "his",
+      b: "hers",
+      c: "its",
+      d: 'theirs'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "21. The group worked as a unit. __________ leader was well-organized.",
+    answers: {
+      a: "Their",
+      b: "Its",
+      c: "His",
+      d: "Her"
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "22. In __________ thirty minutes, I will finish this quiz.",
+    answers: {
+      a: "other",
+      b: "the others",
+      c: "others",
+      d: "another"
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "23. Anyone __________ thinks he got 100% please come to the front.",
+    answers: {
+      a: "who",
+      b: "which",
+      c: "whose",
+      d: "whom"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "24. I didn’t return the book, __________ pages were torn.",
+    answers: {
+      a: "who",
+      b: "of which",
+      c: "of whose",
+      d: "that"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "25. I wasn’t friendly with the supervisor with __________I worked last year.",
+    answers: {
+      a: "whom",
+      b: "whose",
+      c: "who",
+      d: "which"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "26. Ramadan is a time, __________ it is important to think of people who are less fortunate than oneself.",
+    answers: {
+      a: "of which",
+      b: "what",
+      c: "when",
+      d: "of whose"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "27. I put my watch on the table __________ is close to my bed.",
+    answers: {
+      a: "where",
+      b: "which",
+      c: "whose",
+      d: "when"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "28. That is the restaurant __________ they serve Indonesian food.",
+    answers: {
+      a: "which",
+      b: "when",
+      c: "where",
+      d: "who"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "29. Yesterday, I saw a bird __________ to fly with a broken wing.",
+    answers: {
+      a: "tried",
+      b: "which trying",
+      c: "trying",
+      d: "was trying"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "30. I don’t understand __________ soccer.",
+    answers: {
+      a: "why can't you play",
+      b: "why you can't play",
+      c: "you why can't play",
+      d: "you can't play why"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "31. __________ is of no interest to me at all.",
+    answers: {
+      a: "Whether or not you are happy",
+      b: "You are happy whether or not",
+      c: "Whether you are or not happy",
+      d: "Happy whether or not you are"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "32. He doesn’t know when ____________ his mouth.",
+    answers: {
+      a: "should he",
+      b: "shutting",
+      c: "if he should shut",
+      d: "to shut"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "33. Ali said, “I have studied for this quiz.” He told me that he __________ for the quiz.",
+    answers: {
+      a: "has studied",
+      b: "has to study",
+      c: "has been studying",
+      d: "had studied"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "34. Ali asked, “What time does the film begin?” He wondered __________.",
+    answers: {
+      a: "what time did the film begin",
+      b: "what time the film began",
+      c: "what did the time of the film",
+      d: "what time the film had begun"
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "35. There are four kinds of ice cream that are low in fat. You can get __________ one that you want.",
+    answers: {
+      a: "whichever",
+      b: "whereever",
+      c: "however",
+      d: "whenever"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "36. Most chocolates __________ of milk and sugar.",
+    answers: {
+      a: "is consisting",
+      b: "consists",
+      c: "consist",
+      d: "to consist"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "37. Most people __________ when they are very angry.",
+    answers: {
+      a: "shout",
+      b: "are shouting",
+      c: "shouts",
+      d: "shouted"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "38. Paul ____________ lunch at the moment.",
+    answers: {
+      a: "eats",
+      b: "to eat",
+      c: "ate",
+      d: "is eating"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "39. Tom __________ to see a doctor right now.",
+    answers: {
+      a: "to need",
+      b: "is needing",
+      c: "need",
+      d: "needs"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "40. John and Mary	 ____________ their bicycles now.",
+    answers: {
+      a: "riding",
+      b: "rides",
+      c: "are riding",
+      d: "have been riding'"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "41. Paul ________ another book this year.",
+    answers: {
+      a: "writes",
+      b: "is writing",
+      c: "had written",
+      d: "write'"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "42. __________ to improve his English?",
+    answers: {
+      a: "Is Tom trying",
+      b: "Does Tom trying",
+      c: "Have Tom try",
+      d: "Did tom tried"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "43. I __________ English for two hours every night.",
+    answers: {
+      a: "study",
+      b: "am studying",
+      c: "studies",
+      d: "to study"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "44. This food __________ lovely.",
+    answers: {
+      a: "is smelling",
+      b: "are smelling",
+      c: "smells",
+      d: "smell"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "45. I __________ what you told me.",
+    answers: {
+      a: "believing",
+      b: "to believe",
+      c: "believes",
+      d: "believe"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "46. I __________ two hours overtime every day this week.",
+    answers: {
+      a: "did has",
+      b: "had",
+      c: "has",
+      d: "having'"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "47. She __________ people who tell lies.",
+    answers: {
+      a: "hate",
+      b: "is hating",
+      c: "hates",
+      d: "are hating"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "48. He appears__________ asleep.",
+    answers: {
+      a: "be",
+      b: "is",
+      c: "can",
+      d: "to be"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "49. I __________ to the other side of the pool yesterday.",
+    answers: {
+      a: "swim",
+      b: "swimming",
+      c: "am swimming",
+      d: "swam"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "50. While I __________ down the street, it began to snow.",
+    answers: {
+      a: "will walk",
+      b: "walked",
+      c: "walk",
+      d: "was walking"
+    },
+    correctAnswer: 'd'
+  },
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
